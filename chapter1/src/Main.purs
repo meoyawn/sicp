d0ca275c01796649604faf2243040fc014ff39be
adelniz <adelniz@yandex-team.ru>
2015-11-16 11:08:23 +0300

    ridiculous typechecking

--- chapter1/src/Main.purs
+++ chapter1/src/Main.purs
@@ -1,5 +1,11 @@
 module Main where
 
+import Prelude
+import Data.Date
+import Control.Monad.Eff
+import Control.Monad.Eff.Console
+
 import Divisors
 
+main :: forall e. Eff (console :: CONSOLE, now :: Now | e) Unit
 main = timedPrimeTest 19999999
